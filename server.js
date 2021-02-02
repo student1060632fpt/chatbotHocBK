561f488a43583b02a72dedadb7b97b858e03d77d
Nguyễn Lê Xuân Hòa <hoa.nlx@alta.com.vn>
2021-02-02 17:00:43 +0700

    fix server 2

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/webhook', function(req, res) { // Đây là path để validate tooken bên app facebook gửi qua
+  console.log(req.query['hub.verify_token'], req);
   if (req.query['hub.verify_token'] === VALIDATION_TOKEN) {
     res.send(req.query['hub.challenge']);
   }
