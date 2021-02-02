b3b7e96e4324eee7d8787aa90a370d667e1bf6d5
Nguyễn Lê Xuân Hòa <hoa.nlx@alta.com.vn>
2021-02-02 16:13:23 +0700

    second commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const APP_SECRET = '71cfae0265eb6fc6d49daf79d3531368';
+const VALIDATION_TOKEN = 'TokenTuyChon';
+const PAGE_ACCESS_TOKEN = 'EAACBbDxFYzgBAHs0k1PRRGLOGbXiz3s5ilgiCoNVnj6kE5gb3ulkeGSionrD1yxbwYbyFOXW5pFk5kg4AzvgIiMyhmRHZBS22rRfljIcMdnqBfaXZAKhFK8l98jQn9GcdO7dm1hZBnZAk4bm6J1TPcJnhZApvSjBvFfQvX5XxyjpVTaH6I1I5';
+
+var http = require('http');
+var bodyParser = require('body-parser');
+var express = require('express');
+
+var app = express();
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+var server = http.createServer(app);
+var request = require("request");
+
+app.get('/', (req, res) => {
+  res.send("Home page. Server running okay.");
+});
+
+app.get('/webhook', function(req, res) { // Đây là path để validate tooken bên app facebook gửi qua
+  // if (req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    res.send(req.query['hub.challenge']);
+  // }
+  // res.send('Error, wrong validation token');
+});
+
+app.post('/webhook', function(req, res) { // Phần sử lý tin nhắn của người dùng gửi đến
+  var entries = req.body.entry;
+  for (var entry of entries) {
+    var messaging = entry.messaging;
+    for (var message of messaging) {
+      var senderId = message.sender.id;
+      if (message.message) {
+        if (message.message.text) {
+          var text = message.message.text;
+          sendMessage(senderId, "Hello!! I'm a bot. Your message: " + text);
+        }
+      }
+    }
+  }
+  res.status(200).send("OK");
+});
+
+// Đây là function dùng api của facebook để gửi tin nhắn
+function sendMessage(senderId, message) {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: PAGE_ACCESS_TOKEN,
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: senderId
+      },
+      message: {
+        text: message
+      },
+    }
+  });
+}
+
+app.set('port', process.env.PORT || 5000);
+app.set('ip', process.env.IP || "0.0.0.0");
+
+server.listen(app.get('port'), app.get('ip'), function() {
+  console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
+});
